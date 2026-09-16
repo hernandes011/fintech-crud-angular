4ed7314d84047dd971f91d1645c99bbbc49ac59c
PedroHernandes <ph9aguiar@gmail.com>
2026-09-15 22:33:44 -0300

    atualizando pagida de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login: string = "";
+  senha: string = "";
+  botaoDesabilitado: boolean = true;
 
+  onBotaoClicado() {
+    alert("Bem Vindo(a)!")
+  }
+  teclaDigitada(event: KeyboardEvent): void {
+    alert('O usuário digitou: ${event.key}');
+  }
+  validarFormulario(){
+    if(this.login.trim() !=="$$this.senha.trim() !==") {
+      this.botaoDesabilitado = false;
+    } else {
+      this.botaoDesabilitado = true;
+    }
+  }
+  fazerLogin() {
+    if(this.login === 'admin' && this.senha === '123') {
+      alert('Bem Vindo ${this.login}!');
+    } else {
+      alert('Dados inválidos');
+    }
+  }
 }
